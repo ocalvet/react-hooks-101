07cffbefb0f980c17e9a6f8fd03d0d143f115720
Ovidio R. Calvet <calveto@gmail.com>
2019-06-10 00:15:01 -0400

    Get random dog image

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
   const [count, setCount] = useState(0);
+  const [dogImage, setDog] = useState(
+    'https://images.dog.ceo/breeds/mastiff-tibetan/n02108551_1104.jpg'
+  );
+  useEffect(() => {
+    const getDog = async () => {
+      const response = await fetch('https://dog.ceo/api/breeds/image/random');
+      const json = await response.json();
+      setDog(json.message);
+      console.log('got dog', json);
+    };
+    getDog();
+  }, []);
   return (
     <div className="container">
       <h2 class="header">Hooks Testing - 101</h2>
       <h2>Counter - {count}</h2>
       <button onClick={() => setCount(count + 1)}>Increment Count</button>
+      <hr />
+      <img src={dogImage} alt="dog-images" />
     </div>
   );
 }
