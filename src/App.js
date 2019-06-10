8f6479e3dfd9fb6e7a8c31eb2b26db2b6314c0e2
Ovidio R. Calvet <calveto@gmail.com>
2019-06-10 00:05:07 -0400

    Add useState to increment a counter

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [count, setCount] = useState(0);
   return (
     <div className="container">
       <h2 class="header">Hooks Testing - 101</h2>
+      <h2>Counter - {count}</h2>
+      <button onClick={() => setCount(count + 1)}>Increment Count</button>
     </div>
   );
 }
