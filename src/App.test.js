4d150ec6d814ad4a9254422c9e791e86fbdde7c7
Ovidio R. Calvet <calveto@gmail.com>
2019-07-09 21:24:20 -0400

    Update description of the test

--- src/App.test.js
+++ src/App.test.js
@@ -4,7 +4,7 @@ import App from './App';
 
 describe('<App />', () => {
   afterEach(cleanup);
-  it('should render', () => {
+  it('should render a h2 with correct title', () => {
     const { container } = render(<App />);
     const h2 = container.firstChild.firstChild;
     expect(h2.textContent).toBe('Hooks Testing - 101');
