c50df4e4d10da8105fb1e5a4eed4288f20d1fb26
Ovidio R. Calvet <calveto@gmail.com>
2019-07-09 21:23:56 -0400

    Test that the title is correct

--- src/App.test.js
+++ src/App.test.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, cleanup } from '@testing-library/react';
 import App from './App';
 
 describe('<App />', () => {
+  afterEach(cleanup);
   it('should render', () => {
     const { container } = render(<App />);
-    expect(container).toBeDefined();
+    const h2 = container.firstChild.firstChild;
+    expect(h2.textContent).toBe('Hooks Testing - 101');
   });
 });
