66f0a256903f363951fce904548d681a13e4a533
Ovidio R. Calvet <calveto@gmail.com>
2019-07-11 21:42:22 -0400

    Remove console.logs

--- src/useDogs.js
+++ src/useDogs.js
@@ -5,16 +5,13 @@ const useDogs = () => {
     'https://images.dog.ceo/breeds/mastiff-tibetan/n02108551_1104.jpg'
   );
   React.useEffect(() => {
-    console.log('Starting interval');
     const getDog = async () => {
       const response = await fetch('https://dog.ceo/api/breeds/image/random');
       const json = await response.json();
-      console.log('Got image');
       setDog(json.message);
     };
     const interval = setInterval(getDog, 3000);
     return () => {
-      console.log('Cleaning interval');
       clearInterval(interval);
     };
   }, []);
