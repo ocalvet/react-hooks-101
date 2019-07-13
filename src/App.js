4e16fc62039b4551992325afb6a9d14946fd69c6
Ovidio R. Calvet <calveto@gmail.com>
2019-07-13 19:27:03 -0400

    Add more tests

--- src/App.js
+++ src/App.js
@@ -12,10 +12,10 @@ function App() {
     <div className="container">
       <h2 className="header">Hooks Testing - 101</h2>
       <h2>Counter - {count}</h2>
-      <button onClick={() => onUpdateCount(count + 1)}>
-        Increment Count
+      <button onClick={() => onUpdateCount(count + 1)}>Increment Count</button>
+      <button onClick={() => onUpdateCount(count - 1)}>
+        Decrement Count
       </button>{' '}
-      <button onClick={() => onUpdateCount(count - 1)}>Decrement Count</button>{' '}
       <hr />
       <img
         style={{ width: 250, height: 250 }}
--- src/App.test.js
+++ src/App.test.js
@@ -36,15 +36,38 @@ describe('<App />', () => {
     expect(h2.textContent).toBe('Counter - 2');
   });
 
-  it('should draw a button', () => {
+  it('should draw a increment button', () => {
     const { container } = render(<App />);
     const button = container.firstChild.childNodes[2];
     expect(button.textContent).toBe('Increment Count');
   });
 
+  it('should draw a decrement button', () => {
+    const { container } = render(<App />);
+    const button = container.firstChild.childNodes[3];
+    expect(button.textContent).toBe('Decrement Count');
+  });
+
   it('should draw a hr', () => {
     const { container } = render(<App />);
-    const hr = container.firstChild.childNodes[3];
+    const hr = container.firstChild.childNodes[4];
     expect(hr).toMatchSnapshot('<hr />');
+  });
+
+  it('should prevent counter for decrementing pass 0', () => {
+    const { container } = render(<App />);
+    const h2 = container.firstChild.childNodes[1];
+    const btnInc = container.firstChild.childNodes[2];
+    const btnDec = container.firstChild.childNodes[3];
+    expect(h2.textContent).toBe('Counter - 0');
+
+    fireEvent.click(btnInc);
+    expect(h2.textContent).toBe('Counter - 1');
+
+    fireEvent.click(btnDec);
+    expect(h2.textContent).toBe('Counter - 0');
+
+    fireEvent.click(btnDec);
+    expect(h2.textContent).toBe('Counter - 0');
   });
 });
