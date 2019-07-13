4d235a3b1655c520a1370d737528661c3673104c
Ovidio R. Calvet <calveto@gmail.com>
2019-07-12 23:07:41 -0400

    Prevent counter on getting below 0

--- src/App.js
+++ src/App.js
@@ -9,12 +9,17 @@ function App() {
   window.onmessage = function(e) {
     console.log('CHILD MSG RECEIVED', e);
   };
+  const onUpdateCount = newCounter => {
+    if (newCounter >= 0) setCount(newCounter);
+  };
   return (
     <div className="container">
       <h2 className="header">Hooks Testing - 101</h2>
       <h2>Counter - {count}</h2>
-      <button onClick={() => setCount(count + 1)}>Increment Count</button>{' '}
-      <button onClick={() => setCount(count - 1)}>Decrement Count</button>{' '}
+      <button onClick={() => onUpdateCount(count + 1)}>
+        Increment Count
+      </button>{' '}
+      <button onClick={() => onUpdateCount(count - 1)}>Decrement Count</button>{' '}
       <hr />
       <img
         style={{ width: 250, height: 250 }}
